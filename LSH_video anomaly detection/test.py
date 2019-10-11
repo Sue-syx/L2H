ec10707834a4773b97749f03824097345fdd74f8
Sue-syx <54345858+Sue-syx@users.noreply.github.com>
2019-10-11 15:10:58 +0800

    Add files via upload

---
+++ LSH_video anomaly detection/test.py
@@ -0,0 +1,13 @@
+import e2LSH
+from utils import *
+
+
+if __name__ == "__main__":
+
+    C = pow(2, 32) - 5
+    dataSet = readData("test_data.csv")
+    query = [-2.7769, -5.6967, 5.9179, 0.37671, 1]
+
+    indexes = e2LSH.nn_search(dataSet, query, k=20, L=5, r=1, tableSize=20)
+    for index in indexes:
+        print(euclideanDistance(dataSet[index], query))
